3fb81e13460908ba00e11254bcfa10c625fb4724
Evelyn G <evelyn.gmaldonado@gmail.com>
2021-10-31 21:53:17 -0700

    working on seeds folder, creating some used and post data in json and seed function

--- models/User.js
+++ models/User.js
@@ -27,17 +27,17 @@ User.init({
         isAlphanumeric:true
         }
     },
-    password:{
-        type:DataTypes.STRING,
-        validate:{
-            len:[8]
-        }
-    },
     email:{
         type:DataTypes.STRING,
         unique:true,
         validate:{
             isEmail:true
+        }
+    },
+    password:{
+        type:DataTypes.STRING,
+        validate:{
+            len:[8]
         }
     }
 },{
